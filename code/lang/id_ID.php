766e1c2f5ec3e5802af13afce7ea81a32e7a93f8
nyeholt <nyeholt@gmail.com>
2010-08-19 18:56:46 +1000

    Fixed indo translation

--- code/lang/id_ID.php
+++ code/lang/id_ID.php
@@ -3,8 +3,8 @@
 global $lang;
 
 $lang['id_ID']['Pixlr']['ADD_IMAGE'] = 'Tambahkan gambar pakai Pixlr';
-$lang['en_US']['Pixlr']['EXISTING_FILE'] = 'Silahkan pilih selain - file itu sudah ada'; 
-$lang['en_US']['PixlrController']['CLOSE_PIXLR'] = 'Jangan simpan dan tutup';
-$lang['en_US']['PixlrController']['IMAGE_TITLE'] = 'Gambar nama';
-$lang['en_US']['PixlrController']['SAVE_IMAGE'] = 'Simpan gambar';
-$lang['en_US']['PixlrController']['SAVE_TARGET'] = 'Simpan gambar di mana';
+$lang['id_ID']['Pixlr']['EXISTING_FILE'] = 'Silahkan pilih yang lain - file itu sudah ada';
+$lang['id_ID']['PixlrController']['CLOSE_PIXLR'] = 'Jangan simpan dan tutup';
+$lang['id_ID']['PixlrController']['IMAGE_TITLE'] = 'Nama gambar';
+$lang['id_ID']['PixlrController']['SAVE_IMAGE'] = 'Simpan gambar';
+$lang['id_ID']['PixlrController']['SAVE_TARGET'] = 'Simpan gambar di mana?';

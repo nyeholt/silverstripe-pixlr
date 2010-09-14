d908d539c615313b56cf8d6438804a8cabb862e5
nyeholt <nyeholt@gmail.com>
2010-09-14 23:23:06 +1000

    Added nb_NO translation (thanks Fremmedkar)

---
+++ lang/nb_NO.php
@@ -0,0 +1,10 @@
+<?php
+
+global $lang;
+
+$lang['nb_NO']['Pixlr']['ADD_IMAGE'] = 'Legg til bilde med Pixlr';
+$lang['nb_NO']['Pixlr']['EXISTING_FILE'] = 'Den filen eksisterer allerede - vennligst velg et annet navn';
+$lang['nb_NO']['PixlrController']['CLOSE_PIXLR'] = 'Lukk uten &aring; lagre';
+$lang['nb_NO']['PixlrController']['IMAGE_TITLE'] = 'Bildetittel';
+$lang['nb_NO']['PixlrController']['SAVE_IMAGE'] = 'Lagre bilde';
+$lang['nb_NO']['PixlrController']['SAVE_TARGET'] = 'Lagre bilde i';

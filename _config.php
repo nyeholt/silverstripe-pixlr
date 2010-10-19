89da8c049e4ce9f6984a3f80c6091c633df454fb
nyeholt <nyeholt@gmail.com>
2010-10-19 17:35:48 +0700

    MINOR: Added logic to check to ensure pixlr directory installation

--- _config.php
+++ _config.php
@@ -8,3 +8,11 @@ set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__).'/third
 
 Object::add_extension('Image', 'PixlrImageDecorator');
 Object::add_extension('HtmlEditorField_Toolbar', 'PixlrImageFormDecorator');
+
+if (($PIXLR_MODULE_DIR = basename(dirname(__FILE__))) != 'pixlr') {
+	$msg = sprintf(_t(
+			'Pixlr.INCORRECT_MODULE_DIR',
+			"Please place the pixlr module in the pixlr directory in your SilverStripe root, not %s"
+			), $PIXLR_MODULE_DIR);
+	die($msg);
+}
